f347207226ab0f904a7908c9e8211c5dd3979b3f
“midnightblues” <mythmxlcool@vip.qq.com>
2018-12-10 18:24:04 +0800

    bugfix

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -102,29 +102,29 @@ const config = {
 		/**
      * CommonsChunkPlugin
      */
-		// splitChunks: {
-		// 	cacheGroups: {
-		// 		vendor: { // node_modules内的依赖库
-		// 			chunks: "all",
-		// 			test: /[\\/]node_modules[\\/]/,
-		// 			name: "vendor",
-		// 			minChunks: 1, // 被不同entry引用次数(import),1次的话没必要提取
-		// 			maxInitialRequests: 5,
-		// 			minSize: 0,
-		// 			priority: 100,
-		// 			// enforce: true?
-		// 		},
-		// 		common: { // ‘src/js’ 下的js文件
-		// 			chunks: "all",
-		// 			test: /[\\/]src[\\/]js[\\/]/, // 也可以值文件/[\\/]src[\\/]js[\\/].*\.js/,  
-		// 			name: "common", // 生成文件名，依据output规则
-		// 			minChunks: 2,
-		// 			maxInitialRequests: 5,
-		// 			minSize: 0,
-		// 			priority: 1
-		// 		}
-		// 	}
-		// }
+		splitChunks: {
+			cacheGroups: {
+				vendor: { // node_modules内的依赖库
+					chunks: "all",
+					test: /[\\/]node_modules[\\/]/,
+					name: "vendor",
+					minChunks: 1, // 被不同entry引用次数(import),1次的话没必要提取
+					maxInitialRequests: 5,
+					minSize: 0,
+					priority: 100,
+					// enforce: true?
+				},
+				common: { // ‘src/js’ 下的js文件
+					chunks: "all",
+					test: /[\\/]src[\\/]js[\\/]/, // 也可以值文件/[\\/]src[\\/]js[\\/].*\.js/,  
+					name: "common", // 生成文件名，依据output规则
+					minChunks: 2,
+					maxInitialRequests: 5,
+					minSize: 0,
+					priority: 1
+				}
+			}
+		}
 	},
 	plugins: [
 		new MiniCssExtractPlugin({
