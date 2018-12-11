cd9e4e7b0755087da16378d65ea53cb4eb0b99e7
“midnightblues” <mythmxlcool@vip.qq.com>
2018-12-11 11:24:27 +0800

    change test

--- src/web/carousel/test/carousel.test.js
+++ src/web/carousel/test/carousel.test.js
@@ -1,23 +1,24 @@
 import expect from 'expect';
 
-import { Carousel } from '../../../main';
+import {
+	Carousel
+} from '../../../main';
 
 describe('carousel', () => {
-	const carousel = new Carousel({
-		warp: 'w2',
-		main: 'm2',
-		play: true,
-		horizontal: true,
-		point: true,
-		touch: true,
-		pointEleRender(currentIndex) {
-			return `<span data-tap="${currentIndex}">${currentIndex}</span>`;
-		},
-		onSwitch(currentIndex) {
-			console.log(currentIndex);
-		}
-	});
 	it('创建', () => {
-		expect(carousel.init());
+		expect(new Carousel({
+			warp: 'w2',
+			main: 'm2',
+			play: true,
+			horizontal: true,
+			point: true,
+			touch: true,
+			pointEleRender(currentIndex) {
+				return `<span data-tap="${currentIndex}">${currentIndex}</span>`;
+			},
+			onSwitch(currentIndex) {
+				console.log(currentIndex);
+			}
+		}));
 	});
 });
