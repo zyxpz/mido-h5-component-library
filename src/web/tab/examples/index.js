54c4c5eec936b29352c1715569a3b47d57ebfdce
darkyun <1746421850@qq.com>
2019-03-22 18:00:45 +0800

    change: tab

--- src/web/tab/examples/index.js
+++ src/web/tab/examples/index.js
@@ -42,7 +42,25 @@ const data = [
 			},
 			{
 				content: '第一列第一个'
-			}
+			},
+			{
+				content: '第一列第一个'
+			},
+			{
+				content: '第一列第一个'
+			},
+			{
+				content: '第一列第一个'
+			},
+			{
+				content: '第一列第一个'
+			},
+			{
+				content: '第一列第一个'
+			},
+			{
+				content: '第一列第一个'
+			},
 		]
 	},
 	{
@@ -65,7 +83,7 @@ const data = [
 		]
 	},
 	{
-		title: '第三列',
+		title: '第四列',
 		list: [
 			{
 				content: '第三列第一个'
@@ -73,7 +91,7 @@ const data = [
 		]
 	},
 	{
-		title: '第三列',
+		title: '第五列',
 		list: [
 			{
 				content: '第三列第一个'
@@ -81,35 +99,35 @@ const data = [
 		]
 	},
 	{
-		title: '第三列',
+		title: '第六列',
 		list: [
 			{
 				content: '第三列第一个'
 			}
 		]
 	}, {
-		title: '第三列',
+		title: '第7列',
 		list: [
 			{
 				content: '第三列第一个'
 			}
 		]
 	}, {
-		title: '第三列',
+		title: '第8列',
 		list: [
 			{
 				content: '第三列第一个'
 			}
 		]
 	}, {
-		title: '第三列',
+		title: '第9列',
 		list: [
 			{
 				content: '第三列第一个'
 			}
 		]
 	}, {
-		title: '第三列',
+		title: '第10列',
 		list: [
 			{
 				content: '第三列第一个'
@@ -122,16 +140,19 @@ import('../../../main').then(({ Tab }) => {
 		wrap: $('.content'), // 需要添加的元素
 		direction: 'top', // tab所处的方向
 		data, // 传进的数据
-		borderColor: 'blue', // 边框颜色
 		size: '14px', // 字体大小
-		borderSize: '2px', // 边框大小
+		tabListBorder: '1px solid #ddd', // tab列表边框
+		tabBorder: '2px solid blue', // 点击后tab的边框
 		time: 0.5, // 动画时间
 		tabColor: '#ccc', // tab字体颜色
-		height: 200, // tab在两边的时候的设置高度
-		padding: '15px 15px', // 设置tab两边高度
-		istanceToChangeTab: '30%', // 滑动切换宽度
+		height: 375, // tab高度,一般是横向分的时候设置
+		// tabHeight: '125px', // 设置tab两边高度
+		// tabWidth: '60px', // 滑动切换宽度
+		tabHeight: '60px', // 设置tab高度
+		tabWidth: '125px', // 设置tab宽度
 		useOnPan: true, // 是否使用手动拖动
-		tabSwipeable: true, // 是否tab可滑动切换
+		tabIsAnimation: true, // tab切换动画
+		isAnimation: true, // 主要内容区动画
 		tagColor: 'red', // 点击后字体颜色
 		// renderTab: tabInnerTpl({ data, isTab: true }), // 被替换的tab 
 		// renderContent: '<div>content</div>', // tab里面的内容
--- src/web/tab/tab.js
+++ src/web/tab/tab.js
@@ -7,9 +7,9 @@ export default class Tab {
 	constructor(opts) {
 		this.wrap = opts.wrap || '';
 		this.tabColor = opts.tabColor || '#ccc';
-		this.borderColor = opts.borderColor || '#0B71BA';
 		this.size = opts.size || '14px';
-		this.borderSize = opts.borderSize || '';
+		this.tabBorder = opts.tabBorder || 0;
+		this.tabListBorder = opts.tabListBorder || 0;
 		this.direction = opts.direction || '';
 		this.data = opts.data || '';
 		this.onTabClick = opts.onTabClick || loop;
@@ -19,9 +19,15 @@ export default class Tab {
 		this.contentWrap = '';
 		this.time = opts.time || 0.3,
 		this.height = opts.height || 200;
-		this.padding = opts.padding || '';
+		this.tabHeight = opts.tabHeight || '';
 		this.tabSwipeable = opts.tabSwipeable || false;
-		this.istanceToChangeTab = opts.istanceToChangeTab || '';
+		this.tabWidth = opts.tabWidth || '';
+		this.isAnimation = opts.isAnimation || false;
+		this.tabIsAnimation = opts.tabIsAnimation || false;
+		this.moveX = 0;
+		this.moveY = 0;
+		this.tagStyle = {};
+		this.listBorder = 0;
 		this.init();
 	}
 	event() {
@@ -29,22 +35,13 @@ export default class Tab {
 		this.tabTouchmove();
 	}
 	init() {
-		this.horizontal = (this.direction !== 'top' && this.direction !== 'bottom') || false;
+		this.vertical = (this.direction !== 'top' && this.direction !== 'bottom') || false;
 		this.width = this.wrap.width();
-		// this.vertical = this.direction !== 'top' && this.direction !== 'bottom' || false;
-
+		this.horizontal = !this.vertical;
+		this.styleSetting();
 		// tab标签或者底部内容模板设置
 		const tabListDom = this.renderTab || `${tabInnerTpl({ data: this.data, isTab: true, tagColor: this.tagColor })}`;
 		const contentDom = this.contentDom || `${tabInnerTpl({ data: this.data, isList: true })}`;
-		// tab边框设置
-		this.borderSet = this.borderSize ? `${this.borderSetting()}: ${this.borderSize} solid ${this.borderColor};color: ${this.tagColor};padding: ${this.padding}` : `color: ${this.tagColor};padding: ${this.padding}`;
-		// tab自定义样式处理
-		const customizeStyle = {};
-		if (this.padding) {
-			customizeStyle.padding = this.padding;
-		} else if (this.istanceToChangeTab) {
-			customizeStyle.width = this.this.istanceToChangeTab;
-		}
 
 		// 模板渲染
 		this.wrap.html(tabTpl({
@@ -61,18 +58,27 @@ export default class Tab {
 		const tapLists = this.tapContent.children();
 		const lists = this.contentWrap.children();
 		this.tabSlide = this.wrap.find('.J-tap-slide');
+
+		// tab样式处理
 		tapLists.addClass('tap-wrap-common');
-		tapLists[0].style = `${this.borderSet}`;
-		if (this.padding) {
-			tapLists.css({
-				...customizeStyle,
-			});
-		}
 		this.tapContent.css({
-			'width': tapLists.length * tapLists.width()
+			...this.listBorder
+		});
+		$(tapLists[0]).css({
+			...this.tagStyle
+		});
+		tapLists.css({
+			'height': this.tabHeight,
+			'width ': this.tabWidth,
 		});
 
-		if (this.direction === 'top' || this.direction === 'bottom') {
+		// 内容区样式处理
+		// let contentStyle = '';
+		// contentStyle = this.horizontal?
+		if (this.horizontal) {
+			this.tapContent.css({
+				'width': tapLists.length * tapLists.width()
+			});
 			this.contentWrap.css({
 				'width': this.width * lists.length
 			});
@@ -85,8 +91,10 @@ export default class Tab {
 			this.wrap.find('.J-wrap').css({
 				'height': this.height
 			});
+			this.tapContent.css({
+				'height': tapLists.length * tapLists.height()
+			});
 			this.contentWrap.css({
-				'display': 'block',
 				'height': this.height * lists.length,
 			});
 			lists.forEach(item => {
@@ -99,23 +107,48 @@ export default class Tab {
 		}
 		this.event();
 	}
-	borderSetting() {
-		let borderSet = 'border-bottom';
+	styleSetting() {
+		this.tagStyle = {
+			'color': this.tagColor,
+		};
 		switch (this.direction) {
 			case 'bottom':
-				borderSet = 'border-top';
+				this.listBorder = {
+					'border-top': this.tabListBorder,
+				};
+				this.tagStyle = {
+					...this.tagStyle,
+					'border-top': this.tabBorder,
+				};
 				break;
 			case 'left':
-				borderSet = 'border-right';
+				this.listBorder = {
+					'border-right': this.tabListBorder,
+				};
+				this.tagStyle = {
+					...this.tagStyle,
+					'border-right': this.tabBorder,
+				};
 				break;
 			case 'right':
-				borderSet = 'border-left';
+				this.listBorder = {
+					'border-left': this.tabListBorder,
+				};
+				this.tagStyle = {
+					...this.tagStyle,
+					'border-left': this.tabBorder,
+				};
 				break;
 			default:
-				borderSet = 'border-bottom';
+				this.listBorder = {
+					'border-bottom': this.tabListBorder,
+				};
+				this.tagStyle = {
+					...this.tagStyle,
+					'border-bottom': this.tabBorder,
+				};
 				break;
 		}
-		return borderSet;
 	}
 	// tab标签点击
 	tabClick(e) {
@@ -129,47 +162,117 @@ export default class Tab {
 			const $target = $(e.currentTarget);
 			const id = $target.data('id');
 			let moveNum = '';
+			// tab样式设置
+			$target.css({
+				...that.tagStyle
+			});
 			$target.siblings().css({
 				'color': that.color,
 				'border': 0,
 			});
-			$target[0].style = `${that.borderSet}`;
-			if (that.direction && (that.direction === 'top' || that.direction === 'bottom')) {
-				moveNum = (id - 1) * tabWidth;
-				if (id > 0 && id !== tabList.length - 1 && that.tabSwipeable) {
-					that.animation(that.tapContent, moveNum, true);
+			// tab切换，内容区切换
+			if (that.horizontal) {
+				moveNum = -(id - 1) * tabWidth;
+				if (id > 0 && id !== tabList.length - 1) {
+					that.tabAnimation(that.tapContent, moveNum, true);
 					e.stopPropagation();
 				}
 				that.animation(that.contentWrap, that.width * id, true);
 			} else {
-				moveNum = (id - 1) * tabHeight;
-				that.animation(that.tapContent, moveNum);
+				moveNum = -(id - 1) * tabHeight;
+				if (id > 0 && id !== tabList.length - 1) {
+					that.tabAnimation(that.tapContent, moveNum);
+				}
 				that.animation(that.contentWrap, that.height * id);
+
 			}
 		});
 	}
+	// tab拖动函数
 	tabTouchmove() {
 		const that = this;
-		this.tabSlide.on('touchstart', (e) => {
-			// e.stopPropagation();
-			console.log('888');
-			// e.preventDefault();
-			// that.tapContent.css({
-			// 	'transform': `translate3d(0, 0, 0)`,
-			// });
+		let startPosX = 0,
+			startPosY = 0,
+			endPosX = 0,
+			endPosY = 0;
+		// 计算最小移动区域
+		const minMoveY = -this.tapContent.height() + this.height;
+		const minMoveX = -this.tapContent.width() + this.width;
+		this.tapContent.on('touchstart', (e) => {
+
+			// 开始移动，记下移动位置
+			startPosX = e.touches[0].pageX;
+			startPosY = e.touches[0].pageY;
+		}).on('touchmove', function (e) {
+
+			// 移动过程中的操作
+			endPosX = e.touches[0].pageX;
+			endPosY = e.touches[0].pageY;
+			let posLeft = that.tapContent.css('left');
+			posLeft = parseInt(posLeft);
+			let posTop = that.tapContent.css('top');
+			posTop = parseInt(posTop);
+
+			// 计算当前移动的位置
+			that.moveX = posLeft + (endPosX - startPosX);
+			that.moveY = posTop + (endPosY - startPosY);
+			console.log(posLeft, endPosX - startPosX, '000');
+			// 计算移动的区域范围
+			if (that.horizontal && that.moveX <= 0 && that.moveX >= minMoveX) {
+				that.tabAnimation(that.tapContent, that.moveX, true);
+			}
+			if (that.vertical && that.moveY <= 0 && that.moveY >= minMoveY) {
+				that.tabAnimation(that.tapContent, that.moveY);
+			}
 		});
 	}
+	// 主要内容区切换函数
 	animation(animationWrap, istance, horizontal) {
+		const time = this.isAnimation ? this.time : 0;
+		// 横向移动时的动画
 		if (horizontal) {
 			animationWrap.css({
 				'transform': `translate3d(-${istance}px, 0, 0)`,
-				'transition': `transform ${this.time}s`
+				'transition': `transform ${time}s`
 			});
 		} else {
+			// 纵向向移动时的动画
 			animationWrap.css({
 				'transform': `translate3d(0,-${istance}px, 0)`,
-				'transition': `transform ${this.time}s`
+				'transition': `transform ${time}s`
 			});
 		}
 	}
+	// tab切换函数
+	tabAnimation(animationWrap, istance, horizontal) {
+		const time = this.tabIsAnimation ? this.time : 0;
+		// 横向移动时的位置
+		if (horizontal) {
+			animationWrap.css({
+				'left': istance,
+				'top': 0,
+				// 'transition': `left ${time}s`
+			});
+		} else {
+			// 纵向向移动时的位置
+			animationWrap.css({
+				'left': 0,
+				'top': istance,
+				// 'transition': `top ${time}s`
+			});
+		}
+	}
+	// contentStyleHandel() {
+	// 	this.tapContent.css({
+	// 		'width': tapLists.length * tapLists.width()
+	// 	});
+	// 	this.contentWrap.css({
+	// 		'width': this.width * lists.length
+	// 	});
+	// 	lists.forEach(item => {
+	// 		$(item).css({
+	// 			'width': this.width
+	// 		});
+	// 	});
+	// }
 }
